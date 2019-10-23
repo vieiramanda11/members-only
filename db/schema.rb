712812b9b94f9fa02c1f33e51b20614ec2ca497d
vieiramanda11 <amanda_vieira@outlook.com>
2019-10-23 14:23:09 -0300

    adding remember_token for login sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_151004) do
+ActiveRecord::Schema.define(version: 2019_10_23_164724) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_10_23_151004) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "remember_digest"
   end
 
 end
