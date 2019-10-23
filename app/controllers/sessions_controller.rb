58534f594c49ad8045561bcecde59f0792e58bbb
vieiramanda11 <amanda_vieira@outlook.com>
2019-10-23 13:45:10 -0300

    creating logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,18 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def destroy
-  end
-
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_to root_path
     else
-    render 'new'
+      render 'new'
     end
   end
 
+  def destroy
+    log_out
+    redirect_to root_path
+  end
 end
